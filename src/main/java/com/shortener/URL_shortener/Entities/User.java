772a0a5c759adbb1e5dfaf0296dfed45f8607287
comp1 <kiralica01112004@gmail.com>
2023-01-22 16:59:45 +0200

    Packages

--- src/main/java/com/shortener/URL_shortener/Entities/User.java
+++ src/main/java/com/shortener/URL_shortener/Entities/User.java
@@ -1,4 +1,31 @@
 package com.shortener.URL_shortener.Entities;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Table(name = "user")
+@Data
+@NoArgsConstructor
+@ToString
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @NotBlank(message = "Name is mandatory")
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "email")
+    @NotBlank(message = "Email is mandatory")
+    private String email;
+
+    @Column(name = "role")
+    private String role;
+
 }
