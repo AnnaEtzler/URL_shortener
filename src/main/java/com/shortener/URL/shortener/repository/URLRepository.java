25371196d07f9cefd6e4e7d27d8b82a06317bde0
Anna <anna.piskunova@web.de>
2023-01-27 10:27:56 +0100

    repo

---
+++ src/main/java/com/shortener/URL/shortener/repository/URLRepository.java
@@ -0,0 +1,10 @@
+package com.shortener.URL.shortener.repository;
+
+import com.shortener.URL.shortener.models.URL;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+
+public interface URLRepository extends JpaRepository<URL, Long > {
+}
