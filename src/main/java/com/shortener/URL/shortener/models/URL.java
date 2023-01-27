3011c11693d63336cf695d4007ec1490899319c8
Anna <anna.piskunova@web.de>
2023-01-27 09:59:17 +0100

    controller and user

---
+++ src/main/java/com/shortener/URL/shortener/models/URL.java
@@ -0,0 +1,24 @@
+package com.shortener.URL.shortener.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@Setter
+@Getter
+@NoArgsConstructor
+public class URL {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    @Column(name = "shortUrl")
+    private String shortUrl;
+    @Column(name = "longUrl")
+    private String longUrl;
+
+}
