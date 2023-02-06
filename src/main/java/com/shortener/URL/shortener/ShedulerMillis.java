3bfc7af33163bb79f19ee3a7422120bcc6d76f1c
Anna <anna.piskunova@web.de>
2023-02-06 21:39:57 +0100

    scheduler

--- src/main/java/com/shortener/URL/shortener/ShedulerMillis.java
+++ src/main/java/com/shortener/URL/shortener/ShedulerMillis.java
@@ -3,15 +3,19 @@ package com.shortener.URL.shortener;
 import com.shortener.URL.shortener.models.URL;
 import com.shortener.URL.shortener.repository.URLRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@Component
 public class ShedulerMillis {
     @Autowired
     URLRepository urlRepository;
 
-    public void wakeUp()
-        {
+
+    @Scheduled(fixedRate = 300000) // на 5 минут
+    public void wakeUp() {
         long minMillis = System.currentTimeMillis() - 5;  // 30*24*3600
         List<URL> oldUrlsToDelete = urlRepository.findByMillisLessThan(minMillis);
         urlRepository.deleteAll(oldUrlsToDelete);
